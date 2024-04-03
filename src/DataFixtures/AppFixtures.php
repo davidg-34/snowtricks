b33a5e9c6045743d73cbd2a183486939238f66b6
Samantha Halfon Faw <samantha.halfon@gmail.com>
2024-04-03 09:03:46 +0200

    fix fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,18 +2,20 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
 use App\Entity\Tricks;
 use App\Entity\Users;
+use App\Entity\Medias;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use App\Entity\TrickGroup;
 use App\Entity\TrickImage;
 use App\Entity\comments;
-
+use Proxies\__CG__\App\Entity\Medias;
 
 class AppFixtures extends Fixture
 {
@@ -40,10 +42,28 @@ class AppFixtures extends Fixture
                  ->setIsVerified(true);
             $password = $this->hasher->hashPassword($user, 'pass_1234');
             $user->setPassword($password);
-                
             $manager->persist($user);
-
             $users[] = $user;
+        }
+
+        // Create categories
+        $categories = [];
+        for ($i = 0; $i < 3 ; $i++) {
+            $catName = $this->faker->Words(1, true);
+            $category = new Category();
+            $category->setName($catName);
+            $manager->persist($category);
+            $categories[] = $category;
+        }
+
+        // Create random medias
+        $medias = [];
+        for ($i = 0; $i < 3 ; $i++) {
+            $media = new Medias();
+            $media->setMedia($this->faker->image());
+            $media->setType('picture');
+            $manager->persist($media);
+            $medias[] = $media;
         }
 
         // Create tricks
@@ -55,10 +75,11 @@ class AppFixtures extends Fixture
             $trick->setName($name);
             $trick->setDescription($description);
             $trick->setSlug($this->slugger->slug($name));
-            $trick->setMedias($media);
+            $trick->setCategory($categories[array_rand($categories)]);
             $trick->setCreatedAt(\DateTimeImmutable::createFromMutable($this->faker->dateTime));
             $trick->setUpdatedAt(\DateTimeImmutable::createFromMutable($this->faker->dateTime));
             $manager->persist($trick);
+            $trick->addMedia($medias[array_rand($medias)]);
             $tricks[] = $trick;
         }
         //dd($tricks);
