9889001644c44bb7ab0b49a3dcc8d462aec09685
davidg34 <ytnum01@gmail.com>
2024-11-27 19:37:31 +0100

    change accordion by default close

--- public/assets/script.js
+++ public/assets/script.js
@@ -3,6 +3,43 @@ let currentPageTiny = 1; // Pour la version mobile (accordéon)
 
 // Version classique
 async function loadMore() {
+    try {
+        currentPage++;
+        const response = await fetch('/ajax/tricks/' + currentPage);
+        if (!response.ok) throw new Error('Erreur lors du chargement des données.');
+        const html = await response.text();
+        document.getElementById('tricks_container').innerHTML += html;
+    } catch (error) {
+        console.error('Erreur :', error.message);
+    }
+}
+
+// Version tiny (accordéon)
+async function loadMoreTiny() {
+    try {
+        currentPageTiny++;
+        const response = await fetch('/ajax/tricks/' + currentPageTiny);
+        if (!response.ok) throw new Error('Erreur lors du chargement des données.');
+        const html = await response.text();
+        document.getElementById('tricks_container_tiny').innerHTML += html;
+    } catch (error) {
+        console.error('Erreur :', error.message);
+    }
+}
+
+
+
+
+
+
+
+
+
+/* let currentPage = 1; // Pour la version classique
+let currentPageTiny = 1; // Pour la version mobile (accordéon)
+
+// Version classique
+async function loadMore() {
     currentPage++;
     const response = await fetch('/ajax/tricks/' + currentPage);
     const html = await response.text();
@@ -15,4 +52,4 @@ async function loadMoreTiny() {
     const response = await fetch('/ajax/tricks/' + currentPageTiny);
     const html = await response.text();
     document.getElementById('tricks_container_tiny').innerHTML += html;
-}
+} */
